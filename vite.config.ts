58b939b6e700b8e663790564ee6371207f259bff
javier marin <javier.marin@globant.com>
2022-11-26 11:55:35 -0600

    Project basis
    Signed-off-by: javier marin <javier.marin@globant.com>

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/tests/*.spec.ts', 'src/**/*.spec.ts'],
+  },
+  build: {
+    lib: {
+      entry: 'src/my-element.ts',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: /^lit/
+    },
+  }
+})
